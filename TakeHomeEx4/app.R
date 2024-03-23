4705157f46ae59b14bd261535b12bb29249a9e09
ZacharymwMITB <zacharywong.2022@mitb.smu.edu.sg>
2024-03-23 11:49:44 +0800

    Commit after making changes to Ex 9 and Take home 4.1

---
+++ TakeHomeEx4/app.R
@@ -0,0 +1,43 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(tidyverse)
+library(vcd)
+library(ggplot2)
+library(ggmosaic)
+
+Myanmar <- read_csv("data/ACLED_MMR_1.csv")
+
+ui <- fluidPage(
+    titlePanel("Mosaic Plots"),
+    sidebarLayout(
+      varSelectInput("VarSelect1",
+                     "Select Variable 1: ",
+                     data = Myanmar),
+      varSelectInput("varselect2",
+                     "Select Variable 2: ",
+                     data = Myanmar)),
+      mainPanel(
+        plotOutput("Mosaic")
+      )
+)
+
+
+server <- function(input, output) {  
+  output$Mosaic <-renderPlot({
+    vcd::mosaic(~input$VarSelect1 + input$varselect2, data = Myanmar)
+  })
+
+}
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
